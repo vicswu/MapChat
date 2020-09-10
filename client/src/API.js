158e96999555d63db37b64435b946ffd3974c9b2
Victor Shijie Wu <vic_wu1@hotmail.com>
2020-09-09 23:10:26 -0400

    fix api

--- client/src/API.js
+++ client/src/API.js
@@ -1,4 +1,4 @@
-const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:1337' : 'https://travel-log-api.now.sh';
+const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:1337' : 'https://mapchat-live.netlify.app';
 
 export async function listLogEntries() {
     const response = await fetch(`${API_URL}/api/logs`);
